9971b559dfb337e69f0b2cb963e80909d6ff7ee5
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-08 23:35:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,26 @@
 
 def key_for_min_value(name_hash)
 
+  arr=[]
+    #we will find the value of all array and put them in new array
+  arr=name_hash.collect do |key,value|
+     value
+  end
+
+#find minimum
+ c=1
+ min=arr[0]
+ while c < arr.length-1
+   if (min <= arr[c] && arr[c+1] >= min)
+            min=min
+   elsif (min > arr[c] && arr[c] < arr[c+1])
+     min=  arr[c]
+   else
+     min=arr[c+1]
+   end
+   c +=1
+ end
+
+return name_hash.key(min)
+
 end
